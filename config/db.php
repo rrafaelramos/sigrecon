a0a73edec3f48935e4401d9bfd9cb63059061989
rrafaelramos2 <rrafaelramos2@gmail.com>
2020-10-14 19:19:38 -0300

    deploy 1

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=bd_tcc',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'rootifnmg',
+    'password' => 'ifnmg2020',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
